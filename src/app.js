32e7b66e2c3403e447a0c140137ca62dafc8cfaf
Cinthia Gisela Camacho <gis25cam@gmail.com>
2023-01-27 18:09:33 -0600

    remove cors

--- src/app.js
+++ src/app.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongodb = require('./db/connect');
-const cors = require('cors');
 
 const port = process.env.PORT || 8080;
 const app = express();
@@ -10,7 +9,6 @@ const swaggerUi = require('swagger-ui-express');
 const swaggerDocument = require('./swagger.json');
 
 app
-.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
   .use(bodyParser.json())
   .use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
