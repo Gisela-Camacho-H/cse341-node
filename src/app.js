a13f2835c9c68f564f4fdaa33bdf3dcbd6829a7e
Cinthia Gisela Camacho <gis25cam@gmail.com>
2023-01-27 19:34:53 -0600

    add res.serHeader

--- src/app.js
+++ src/app.js
@@ -12,6 +12,10 @@ app
   .use(bodyParser.json())
   .use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control_Allow-Headers', 
+    'Origin, X-Requested-With, Content-Type, Accept, Z-Key');
+    res.setHeader('Content-Type', 'application/json');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
     next();
   })
   .use('/', require('./routes'));
